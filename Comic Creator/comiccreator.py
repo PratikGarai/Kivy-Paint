f4f3ce726f4340d58c0d40e36feebf21794b9085
Pratik Garai <pratikgarai0208@gmail.com>
2020-05-08 10:49:45 +0530

    Started touch event

--- Comic Creator/comiccreator.py
+++ Comic Creator/comiccreator.py
@@ -8,8 +8,8 @@ Config.set('graphics','width',500)
 
 Builder.load_file('comicwidgets.kv')
 Builder.load_file('comiccreator.kv')
-Builder.load_file('toolbox.kv')
 Builder.load_file('drawingspace.kv')
+Builder.load_file('toolbox.kv')
 Builder.load_file('statusbar.kv')
 Builder.load_file('generaloptions.kv')
 
